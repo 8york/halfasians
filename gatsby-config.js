9df71ff45dab4c462917950fea780bc13ecd8a96
Akiko Yamasaki <paonpaon@gmail.com>
2023-06-05 16:02:17 +1000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `halfasianweb`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
