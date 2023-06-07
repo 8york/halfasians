597cdb2b472d41beb664933781ca80902d7ba6b3
Akiko Yamasaki <paonpaon@gmail.com>
2023-06-08 09:53:51 +1000

    css animation and grid added

--- src/components/images.js
+++ src/components/images.js
@@ -2,50 +2,55 @@ import * as React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import {
-        imagesContainer
-} from './images.module.css';
+  imagesContainer,
+  galleryImg,
+  galleryTwo,
+  galleryPtag,
+} from "./images.module.css";
 
-
-
-const ImageGallery = ()=>{
-    const data = useStaticQuery(graphql`
-    query{
-        allImagesJson {
-          nodes {
-            images {
-              alt
-              image {
-                childImageSharp {
-                  gatsbyImageData(
-                    formats: AUTO
-                    aspectRatio: 1
-                    height: 250
-                    transformOptions: {fit: COVER}
-                  )
-                }
+const ImageGallery = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allImagesJson {
+        nodes {
+          images {
+            alt
+            image {
+              childImageSharp {
+                gatsbyImageData(
+                  formats: AUTO
+                  aspectRatio: 1
+                  height: 250
+                  transformOptions: { fit: COVER }
+                )
               }
             }
           }
         }
       }
-    `)
-    const imageData = data.allImagesJson.nodes[0]
-    return(
-        <section>
-    <p>does it display?</p>
-        <div className={imagesContainer}>
-            {imageData.images.map((image, index)=>{
-                return(
-                <div key ={index}>
-                    <GatsbyImage image={getImage(image.image)} alt={image.alt}  />
-                </div>
-                )
-            })}
-        </div>
-
-    </section>
-    )
     }
+  `);
+  const imageData = data.allImagesJson.nodes[0];
+  return (
+    <section>
+      <h2 className={galleryTwo}>What a Gallery</h2>
+      <p className={galleryPtag}>CLICK ON!</p>
+      <div className={imagesContainer}>
+        {imageData.images.map((image, index) => {
+          return (
+            <div key={index}>
+              <GatsbyImage
+                image={getImage(image.image)}
+                alt={image.alt}
+                className={galleryImg}
+              />
+            </div>
+          );
+        })}
+      </div>
+     
+    </section>
+  );
+};
 
-
-    export default ImageGallery
+export default ImageGallery;
