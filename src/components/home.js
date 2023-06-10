ca86d307220b651312e1b8dde243e4f027ed6659
Akiko Yamasaki <paonpaon@gmail.com>
2023-06-10 17:34:38 +1000

    css wip

--- src/components/home.js
+++ src/components/home.js
@@ -12,6 +12,7 @@ import {
   anotherDc,
   anotherEightYork,
   secondImg,
+  isItMusic,
 } from "./home.module.css";
 
 const Home = () => {
@@ -22,18 +23,15 @@ const Home = () => {
         <div className={a}>A</div>
       </div>
 
-      
       {/* render images/ not dynamic with staticImage plugin */}
       <div className={animatedImage}>
-      <p>Is it music?</p>
-      <p>where is the music?</p>
+        <p className={isItMusic}>Is it music?</p>
+        {/* <p className={isItMusic}>where is the music?</p> */}
         <StaticImage
           className={dc}
           alt="DC dancing all over the screen"
           src="../../assets/images/3HalfAsian.png"
         />
-      {/* </div> */}
-      {/* <div className={animatedImage}> */}
         <StaticImage
           className={eightyork}
           alt="8 york dancing"
@@ -41,19 +39,18 @@ const Home = () => {
         />
         {/* <div> */}
         <div className={secondImg}>
-        <StaticImage
-          className={anotherDc}
-          alt="dc standing"
-          src="../../assets/images/5HalfAsian.png"
-        />
-        <StaticImage
-          className={anotherEightYork}
-          alt="8 york dancing"
-          src="../../assets/images/2HalfAsian.png"
-        />
+          <StaticImage
+            className={anotherDc}
+            alt="dc standing"
+            src="../../assets/images/2HalfAsian.png"
+          />
+          <StaticImage
+            className={anotherEightYork}
+            alt="8 york dancing"
+            src="../../assets/images/1HalfAsian.png"
+          />
         </div>
       </div>
-      
     </div>
   );
 };
