0caf1a1ba8d5a52d0b423494452bf5b7f7cd28fc
Akiko Yamasaki <paonpaon@gmail.com>
2023-06-13 22:27:08 +1000

    body margin taken

--- src/components/Performance-info.js
+++ src/components/Performance-info.js
@@ -18,11 +18,11 @@ import { videoContainer, videoWrap, vimeo } from "./video.module.css";
 const videos = [
   {
     videoUrl: "https://www.youtube.com/embed/SWlkk6X15ho",
-    text: "First time ever playing with sound system and microphone at Brunswick Ballroom supporting <a href='https://monnonealone.bandcamp.com/' target='_blank'>Monnone Alone</a> between lockdowns in 2021. How could we play sold out show under strict pandemic rules and restrictions that we were sure that the show will be canceled. ",
+    text: "First time ever playing with sound system and microphone at Brunswick Ballroom supporting <a href='https://monnonealone.bandcamp.com/' target='_blank' rel='noreferrer'>Monnone Alone</a> between lockdowns in 2021. How could we play sold out show under strict pandemic rules and restrictions that we were sure that the show will be canceled. ",
   },
   {
     videoUrl: "https://www.youtube.com/embed/V_KCngNhyu8",
-    text: "We lost all our senses for a little while when our inspiration <a href='https://burntsausages.bandcamp.com/album/the-white-bread-album' target='_blank'>The burnt Sausages</a> invited us to play at a show with them. Still living with the hype and for ever."
+    text: "We lost all our senses for a little while when our inspiration <a href='https://burntsausages.bandcamp.com/album/the-white-bread-album' target='_blank' rel='noreferrer'>The burnt Sausages</a> invited us to play at a show with them. Still living with the hype and for ever.",
   },
 ];
 const Performance = () => {
@@ -89,11 +89,19 @@ const Performance = () => {
             allowfullscreen
           ></iframe>
           <p className={vimeo}>
-            The First time ever performance for Halfasian was at a group exhibition 2Q18 at Testing
-            Grounds curated by <a href="https://www.thewanderingroom.org/" target='_blank'>the wandering room</a> in early 2018. This group exhibition was themed around a
-            novel, 1Q84, by Haruki Murakami. Halfasian made 6 songs inspired by
-            surreal but realistic novel and performed at the opening event
-            walking around and played at unregular intervals
+            The First time ever performance for Halfasian was at a group
+            exhibition 2Q18 at Testing Grounds curated by{" "}
+            <a
+              href="https://www.thewanderingroom.org/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              the wandering room
+            </a>{" "}
+            in early 2018. This group exhibition was themed around a novel,
+            1Q84, by Haruki Murakami. Halfasian made 6 songs inspired by surreal
+            but realistic novel and performed at the opening event walking
+            around and played at unregular intervals
           </p>
         </div>
         {/* rendering youtube videos using the data */}
@@ -106,13 +114,13 @@ const Performance = () => {
               title={video.title}
               className={video}
             />
-            <p dangerouslySetInnerHTML={{__html: video.text}}></p>
+            <p dangerouslySetInnerHTML={{ __html: video.text }}></p>
             {/* <p>{video.text}</p> */}
           </div>
         ))}
       </div>
-          </>
-      )
-      }
+    </>
+  );
+};
 
 export default Performance;
